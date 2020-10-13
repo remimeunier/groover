b6ca141fdf99189efacf753106e5d602d5ff2ac0
remimeunier <meunier.remi.p@gmail.com>
2020-10-13 18:12:04 -0500

    Add artists specifications

--- api/models.py
+++ api/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Artist(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=100)
+    last_release_name = models.CharField(max_length=100)
+    last_release_date = models.DateTimeField()
+    genres = models.ManyToManyField(Genre, blank=True, related_name='artists')
+    spotify_link = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
