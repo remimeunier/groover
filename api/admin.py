0b9ce58b4f35f0739ede21ef70b274504aca6744
remimeunier <meunier.remi.p@gmail.com>
2020-10-14 11:26:39 -0500

    Ingest Genrs of artists

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Artist, Genre
 
 admin.site.register(Artist)
+admin.site.register(Genre)
--- artistsIngestion/ingester.py
+++ artistsIngestion/ingester.py
@@ -1,11 +1,10 @@
-from api.models import Artist
+from api.models import Artist, Genre
 from artistsIngestion.spotifyApi import SpotifyApi
 
 class Reached(Exception): pass
 
 def update_artists():
     spotify = SpotifyApi()
-
     try:
         # Fetch all new releases from spotify and iterate
         for release in spotify.get_new_release():
@@ -21,12 +20,20 @@ def update_artists():
                     in_base_artist.last_release_name = release['name']
                     in_base_artist.last_release_date = release['release_date']
                     in_base_artist.save()
-                # if not : create it
+                # If not : create it
                 else:
                     artist = spotify.get_artist(artist['id'])
-                    Artist(name=artist['name'], last_release_name=release['name'],
-                           last_release_date=release['release_date'],
-                           spotify_link=artist['external_urls']['spotify']).save()
+                    # Get all genres of the artist
+                    genres_artist = []
+                    for genre in artist['genres']:
+                        in_base_genre, c = Genre.objects.get_or_create(name=genre)
+                        genres_artist.append(in_base_genre)
+                    a = Artist(name=artist['name'], last_release_name=release['name'],
+                               last_release_date=release['release_date'],
+                               spotify_link=artist['external_urls']['spotify'])
+                    a.save()
+                    a.genres.set(genres_artist)
+                    a.save()
     except Reached:
-      pass # stop everything if we reached an already ingested release
+      pass # Stop everything if we reached an already ingested release
 
