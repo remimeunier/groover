ff469d03a71935f45f8820d69d7e4dddc383e50c
remimeunier <meunier.remi.p@gmail.com>
2020-10-14 11:30:26 -0500

    Add genres in api

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,18 @@
 # serializers.py
 from rest_framework import serializers
 
-from .models import Artist
+from .models import Artist, Genre
+
+
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name',)
+
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+
     class Meta:
         model = Artist
-        fields = ('name', 'last_release_name', 'last_release_date', 'spotify_link')
+        fields = ('name', 'last_release_name', 'last_release_date', 'spotify_link', 'genres')
