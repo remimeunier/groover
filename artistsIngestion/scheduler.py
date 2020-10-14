a4824b0e7d1c556c63af04eb12153c65d1ee3a07
remimeunier <meunier.remi.p@gmail.com>
2020-10-14 16:26:48 -0500

    Extract frenquenzy of update

--- artistsIngestion/scheduler.py
+++ artistsIngestion/scheduler.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from artistsIngestion import ingester
 
+INTERVAL_OF_UPDATE = 1 # in minute
+
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(ingester.update_artists, 'interval', minutes=1)
+    scheduler.add_job(ingester.update_artists, 'interval', minutes=INTERVAL_OF_UPDATE)
     scheduler.start()
