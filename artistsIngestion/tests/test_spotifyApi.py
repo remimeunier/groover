4663ffd51427aa9bc72b346287e26f966b796e4f
remimeunier <meunier.remi.p@gmail.com>
2020-10-16 11:52:57 +0200

    FInish testing

--- artistsIngestion/tests/test_spotifyApi.py
+++ artistsIngestion/tests/test_spotifyApi.py
@@ -1,21 +1,80 @@
-# import pytest
-# from unittest.mock import Mock, patch
+import pytest
+from unittest.mock import Mock, patch
 
-# from spotifyAuth.models import SpotifyAuth
-# from artistsIngestion.spotifyApi import SpotifyApi
-# from artistsIngestion.tests.fixtures import (
-#     news_release_from_spotify,
-#     artist_from_spotify,
-# )
+from api.tests.fixtures import dummy_auth
+from spotifyAuth.models import SpotifyAuth
+from artistsIngestion.spotifyApi import SpotifyApi, SpotifyErrorMessage
+from artistsIngestion.tests.fixtures import (
+    news_release_from_spotify,
+    artist_from_spotify,
+)
 
-# class TestSpotifyApi:
+class TestSpotifyApi:
 
-#     @pytest.mark.django_db
-#     def test_get_new_release(self, artist_from_spotify, news_release_from_spotify):
-#         spotify_auth = SpotifyAuth(token="something", refresh_token="refresh something")
-#         spotify_auth.save()
-#         api = SpotifyApi(auth=spotify_auth)
-#         return_val = { 'albums': { 'items': news_release_from_spotify }}
-#         with patch('requests.get', return_value=return_val):
-#             result = api.get_new_release()
-#         assert resul == news_release_from_spotify
+    @pytest.mark.django_db
+    def test_get_new_release(self, dummy_auth, news_release_from_spotify):
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, dummy_auth)
+        mock_response = Mock()
+        mock_response.json.return_value = { 'albums': { 'items': news_release_from_spotify } }
+        with patch('requests.get', return_value=mock_response):
+            result = api.get_new_release()
+        assert result == news_release_from_spotify
+
+    @pytest.mark.django_db
+    def test_get_artist(self, dummy_auth, artist_from_spotify):
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, dummy_auth)
+        mock_response = Mock()
+        mock_response.json.return_value = artist_from_spotify
+        with patch('requests.get', return_value=mock_response):
+            result = api.get_artist('id')
+        assert result == artist_from_spotify
+
+    @pytest.mark.django_db
+    def test_spotify_error_on_artist(self, dummy_auth):
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, dummy_auth)
+        mock_response = Mock()
+        mock_response.json.return_value = { 'error': { 'message' : 'Not available' } }
+        with patch('requests.get', return_value=mock_response):
+            with pytest.raises(SpotifyErrorMessage):
+                result = api.get_artist('id')
+
+    @pytest.mark.django_db
+    def test_spotify_error_on_release(self, dummy_auth):
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, dummy_auth)
+        mock_response = Mock()
+        mock_response.json.return_value = { 'error': { 'message' : 'Not available' } }
+        with patch('requests.get', return_value=mock_response):
+            with pytest.raises(SpotifyErrorMessage):
+                result = api.get_new_release()
+
+    @pytest.mark.django_db
+    def test_get_new_release_with_token_changes(self, news_release_from_spotify):
+        mock_auth = Mock()
+        mock_auth.token.return_value = 'something'
+        mock_auth._handle_token_expired.return_value = mock_auth
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, mock_auth)
+        mock_response = Mock()
+        mock_response.json.side_effect = [ { 'error': { 'message' : 'The access token expired' } },
+                                           { 'albums': { 'items': news_release_from_spotify } } ]
+        with patch('requests.get', return_value=mock_response):
+            result = api.get_new_release()
+        assert result == news_release_from_spotify
+
+    @pytest.mark.django_db
+    def test_get_artist_with_token_changes(self, artist_from_spotify):
+        mock_auth = Mock()
+        mock_auth.token.return_value = 'something'
+        mock_auth._handle_token_expired.return_value = mock_auth
+        api =  object.__new__(SpotifyApi)
+        SpotifyApi.__init__(api, mock_auth)
+        mock_response = Mock()
+        mock_response.json.side_effect = [ { 'error': { 'message' : 'The access token expired' } },
+                                           artist_from_spotify ]
+        with patch('requests.get', return_value=mock_response):
+            result = api.get_artist('id')
+        assert result == artist_from_spotify
--- spotifyAuth/tests/test_spotifyAuthApi.py
+++ spotifyAuth/tests/test_spotifyAuthApi.py
@@ -1,4 +1,5 @@
 import pytest
+from django.conf import settings
 from rest_framework import status
 from unittest.mock import Mock, patch
 
@@ -23,3 +24,7 @@ class TestSpotifyAuthApi:
         with patch('requests.post', return_value=mock_response):
             token = SpotifyAuthApi().refreshAuth('thing')
         assert token['refresh_token'] == 'thing'
+
+    @pytest.mark.django_db
+    def test_redirection_uri(self):
+        assert SpotifyAuthApi().getUser() == "https://accounts.spotify.com/authorize/?client_id={}&redirect_uri=http://localhost:5000/auth/callback&scope=user-read-email user-read-private&response_type=code".format(settings.SPOTIFY_CLIENT_ID)
